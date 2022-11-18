be8f133535100fce115baed5ee12bec0a2ff2531
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-11-19 05:01:21 +0900

    ゲーム終了後にポイントをリセット

--- src/main/java/love/chihuyu/commands/ItemhuntStart.kt
+++ src/main/java/love/chihuyu/commands/ItemhuntStart.kt
@@ -102,6 +102,7 @@ object ItemhuntStart {
                     }
 
                     TargetItem.activeTarget.clear()
+                    PlayerData.data.clear()
                     ScoreboardUtil.updateServerScoreboard()
                 }
 
