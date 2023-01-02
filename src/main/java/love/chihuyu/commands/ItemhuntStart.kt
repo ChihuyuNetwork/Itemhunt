23f65b78045801379c0d3cf1fd4b18b8cd54398d
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2023-01-03 07:09:56 +0900

    時間とフェーズをプリセット化した

--- src/main/java/love/chihuyu/commands/ItemhuntStart.kt
+++ src/main/java/love/chihuyu/commands/ItemhuntStart.kt
@@ -1,23 +1,33 @@
 package love.chihuyu.commands
 
 import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.arguments.ArgumentSuggestions
+import dev.jorel.commandapi.arguments.StringArgument
 import dev.jorel.commandapi.executors.PlayerCommandExecutor
 import love.chihuyu.Itemhunt.Companion.plugin
 import love.chihuyu.Itemhunt.Companion.prefix
 import love.chihuyu.config.ConfigKeys
 import love.chihuyu.game.GameManager
+import love.chihuyu.game.TimePresets
 import love.chihuyu.utils.EpochUtil
 
 object ItemhuntStart {
 
-    val main: CommandAPICommand = CommandAPICommand("start").executesPlayer(
-        PlayerCommandExecutor { sender, _ ->
+    val main: CommandAPICommand = CommandAPICommand("start")
+        .withArguments(StringArgument("時間").replaceSuggestions(ArgumentSuggestions.strings {
+            TimePresets.values().map { it.name }.toTypedArray()
+        }))
+        .executesPlayer(PlayerCommandExecutor { sender, args ->
             val startedEpoch = EpochUtil.nowEpoch()
-            val phases = plugin.config.getInt(ConfigKeys.PHASES.key)
-            val secondsPerPhase = plugin.config.getLong(ConfigKeys.PHASE_TIME.key)
+            val timePresets = try {
+                TimePresets.valueOf(args[0] as String)
+            } catch (e: IllegalArgumentException) {
+                sender.sendMessage("$prefix 無効な時間指定です")
+                return@PlayerCommandExecutor
+            }
             val materialCategories = plugin.config.getStringList(ConfigKeys.MATERIALS.key)!!
             val targets = plugin.config.getInt(ConfigKeys.TARGETS.key)
-            val gameFinishEpoch = startedEpoch + (secondsPerPhase * phases)
+            val gameFinishEpoch = startedEpoch + timePresets.seconds
             val clearItem = plugin.config.getBoolean(ConfigKeys.CLEAR_ITEM.key)
             val nightVision = plugin.config.getBoolean(ConfigKeys.NIGHT_VISION.key)
             val keepInventory = plugin.config.getBoolean(ConfigKeys.KEEP_INVENTORY.key)
@@ -49,8 +59,8 @@ object ItemhuntStart {
             }
 
             GameManager.startedEpoch = startedEpoch
-            GameManager.phases = phases
-            GameManager.secondsPerPhase = secondsPerPhase
+            GameManager.phases = timePresets.seconds.toInt() / 300
+            GameManager.secondsPerPhase = 300
             GameManager.materialCategories = materialCategories
             GameManager.targets = targets
             GameManager.gameFinishEpoch = gameFinishEpoch
---
+++ src/main/java/love/chihuyu/game/TimePresets.kt
@@ -0,0 +1,8 @@
+package love.chihuyu.game
+
+enum class TimePresets(val seconds: Long) {
+    TEN_MIN(600),
+    HALF_HOUR(1800),
+    ONE_HOUR(3600),
+    TWO_HOUR(7200)
+}
